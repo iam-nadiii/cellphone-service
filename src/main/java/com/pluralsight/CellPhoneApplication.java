560d67112d6c17e6a51671cbc3a874f6481e60f4
iam-nadiii <naodd43@gmail.com>
2026-04-14 13:38:32 -0500

    finished the cell phone app by refactoring all methods and rename my app to CellPhoneApplication

---
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,78 @@
+package com.pluralsight;
+
+
+import java.util.Scanner;
+
+public class CellPhoneApplication {
+
+    public static void main(String[] args) {
+        CellPhone cellphone = getCellPhone();
+
+
+        displayInfo(cellphone);
+
+
+    }
+
+    private static CellPhone getCellPhone() {
+        Scanner input = new Scanner(System.in);
+
+        CellPhone cellphone = new CellPhone();
+
+        prompt("Enter serial number: ");
+        cellphone.setSerialNumber(input.nextLong());
+        input.nextLine();
+
+        prompt("Enter the model of your phone: ");
+        cellphone.setModel(input.nextLine());
+//        input.nextLine();
+
+        prompt("Enter your carrier: ");
+        cellphone.setCarrier(input.next());
+        input.nextLine();
+
+
+        prompt("Enter your phone number: ");
+        cellphone.setPhoneNumber(input.next());
+        input.nextLine();
+
+        prompt("Enter name of the owner of this number: ");
+        cellphone.setOwner(input.next());
+        input.nextLine();
+        return cellphone;
+    }
+
+    private static void displayInfo(CellPhone cellphone) {
+        System.out.println();
+        System.out.print("Owner name: ");
+        prompt(cellphone.getOwner());
+        System.out.println();
+
+        System.out.print("Phone number: ");
+        prompt(cellphone.getPhoneNumber());
+        System.out.println();
+
+        System.out.print("Model: ");
+        prompt(cellphone.getModel());
+        System.out.println();
+
+        System.out.print("Serial Number: ");
+        prompt(Long.toString(cellphone.getSerialNumber()));
+        System.out.println();
+
+        System.out.print("Carrier: ");
+        prompt(cellphone.getCarrier());
+        System.out.println();
+
+
+    }
+
+    static void prompt(String label){
+        System.out.print(label);
+    }
+}
+
+
+
+
+
